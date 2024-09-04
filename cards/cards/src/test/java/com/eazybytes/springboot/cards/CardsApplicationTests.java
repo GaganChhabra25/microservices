1c378685c979096ced56e4469c1ef4463cd96539
Gagan Chhabra <gagan.chhabra1990@gmail.com>
2024-09-04 13:26:23 +0530

    Change loans folder name

--- cards/cards/src/test/java/com/eazybytes/springboot/cards/CardsApplicationTests.java
+++ cards/cards/src/test/java/com/eazybytes/springboot/cards/CardsApplicationTests.java
@@ -1,10 +1,10 @@
-package com.eazybytes.springboot.accounts;
+package com.eazybytes.springboot.cards;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AccountsApplicationTests {
+class CardsApplicationTests {
 
 	@Test
 	void contextLoads() {
